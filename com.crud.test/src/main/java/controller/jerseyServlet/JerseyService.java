62c69aef23912f0df9db21257b05dff83be69103
cardinal <kardinalmail@gmail.com>
2019-09-11 22:33:37 +0300

    unnecessary removal

--- com.crud.test/src/main/java/controller/jerseyServlet/JerseyService.java
+++ com.crud.test/src/main/java/controller/jerseyServlet/JerseyService.java
@@ -1,29 +1,21 @@
 package controller.jerseyServlet;
 
-
-
 import model.PriceData;
 import service.ServicePriceData;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-
 @Path("/check")
 public class JerseyService {
 
-    ServicePriceData servicePriceData=new ServicePriceData();
+    ServicePriceData servicePriceData = new ServicePriceData();
     List<PriceData> priceDataList = servicePriceData.findAllProducts();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<PriceData> checkData () {
-
-
+    public List<PriceData> checkData() {
         System.out.println("RESTful Service running Test");
         return priceDataList;
     }
-
-
 }
